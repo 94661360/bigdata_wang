b3fcdc8075579630f768e6dfd3cdac11a33b91b4
94661360 <94661360@qq.com>
2016-03-31 17:35:45 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+first project 
+wordcount
+20160331
---
+++ start/src/helloworld.scala
@@ -0,0 +1,10 @@
+/**
+ * Created by root on 16-3-23.
+ */
+object helloworld {
+  def main(args:Array[String]): Unit =
+  {
+    println("helloworld")
+  }
+
+}
